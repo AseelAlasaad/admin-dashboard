15c917d42bba7fcf04da6bfed5d010c9b2e58564
AseelAlasaad <aseelalasaad@gmail.com>
2025-01-07 00:01:25 +0300

    build server

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+const app= require('./src/server')
+const port=3005
+
+app.start(port|| 3001)
---
+++ src/server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express= require('express')
+const cors= require('cors')
+
+
+const app= express();
+app.use(express.json())
+
+const handel404 = require ('./auth/handlers/404')
+const handel500= require('./auth/handlers/500')
+
+app.get('/', (req, res)=>{
+    res.send("Home page")
+    
+})
+
+app.use(handel404)
+app.use(handel500)
+
+module.exports={
+    server:app,
+    start: (port)=>{
+        app.listen(port,()=>{
+        console.log(`Server up on ${port}`)
+    })
+
+}
+}
